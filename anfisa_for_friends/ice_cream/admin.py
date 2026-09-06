ecd415235c8efab7f83d1eb2f061ad5b7d2e5aaa
Anastasia Ermokhina <Ananasks@users.noreply.github.com>
2026-09-06 04:50:48 +0300

    Настроить модели и админку каталога мороженого

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, IceCream, Topping, Wrapper
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper',
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category',
+    )
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+# Регистрируем классы с настройками админки для моделей IceCream и Category.
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+# Для Topping и Wrapper используется стандартный интерфейс админки.
+admin.site.register(Topping)
+admin.site.register(Wrapper)
